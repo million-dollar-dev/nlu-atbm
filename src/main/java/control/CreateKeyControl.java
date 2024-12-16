ed4d744fd0f6f082288817098968d74b22b157c5
million-dollar-dev <hoangtuan1304@hotmail.com>
2024-12-16 22:37:25 +0700

    feat: create key

---
+++ src/main/java/control/CreateKeyControl.java
@@ -0,0 +1,73 @@
+package control;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import util.Helpers;
+
+import java.io.IOException;
+
+import dao.KeyDAO;
+import dao.UserDAO;
+import entity.User;
+import entity.UserKey;
+
+@WebServlet("/createKey")
+public class CreateKeyControl extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CreateKeyControl() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String userId = request.getParameter("userId");
+		System.out.println(userId);
+		String algo = request.getParameter("algoKey");
+		int keySize = Integer.valueOf(request.getParameter("keySize"));
+		String expireStr = request.getParameter("expireTime");
+		int expireTime = expireStr == "" ? 0 : Integer.valueOf(expireStr);
+		KeyDAO keyDao = new KeyDAO();
+		try {
+			String[] key = Helpers.generateKeyPair(algo, keySize);
+			String publicKey = key[0];
+			String secretKey = key[1];
+			int keyId;
+			if (expireTime != 0)
+				keyId = keyDao.createKey(userId, publicKey, algo, keySize, expireTime);
+			else
+				keyId = keyDao.createKey(userId, publicKey, algo, keySize);
+			UserKey createdKey = keyDao.getKeyActiveByKeyId(keyId);
+			
+			request.setAttribute("publicKey", publicKey);
+			request.setAttribute("secretKey", secretKey);
+			request.setAttribute("createdKey", createdKey);
+			request.getRequestDispatcher("your-info.jsp").forward(request, response);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+}
--- src/main/java/dao/KeyDAO.java
+++ src/main/java/dao/KeyDAO.java
@@ -3,7 +3,10 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -17,10 +20,10 @@ public class KeyDAO {
 	PreparedStatement ps = null;
 	ResultSet rs = null;
 
-	public UserKey getKeyByUserId(int id) {
-		String query = "select * from userPublicKeys where UserId = " + id;
+	public UserKey getKeyActiveByUserId(int id) {
+		String query = "select * from userPublicKeys where UserId = " + id + " and isActive = 'Active'";
 		Timestamp timestamp = null;
-		Date createdAt = null, updatedAt = null, expiresAt = null;
+		Date createdAt = null, expiresAt = null;
 		try {
 			conn = DBContext.getInstance().getConnection();
 			ps = conn.prepareStatement(query);
@@ -30,18 +33,13 @@ public class KeyDAO {
 				if (timestamp != null) {
 					createdAt = new Date(timestamp.getTime());
 				}
-				timestamp = rs.getTimestamp("UpdatedAt");
-				if (timestamp != null) {
-					updatedAt = new Date(timestamp.getTime());
-				}
-
 				timestamp = rs.getTimestamp("ExpiresAt");
 				if (timestamp != null) {
 					expiresAt = new Date(timestamp.getTime());
 				}
 				boolean isActive = rs.getString("IsActive").equalsIgnoreCase("Active") ? true : false;
 				return new UserKey(Integer.toString(id), rs.getString("PublicKey"), rs.getString("KeyType"),
-						rs.getInt("KeySize"), createdAt, updatedAt, expiresAt, isActive);
+						rs.getInt("KeySize"), createdAt, expiresAt, isActive);
 			}
 		} catch (Exception e) {
 			System.out.println(e);
@@ -49,24 +47,106 @@ public class KeyDAO {
 		return null;
 	}
 
-	public Category getCategoryById(int id) {
-		String query = "select * from categories where CategoryID = ?";
+	public UserKey getKeyActiveByKeyId(int id) {
+		String query = "select * from userPublicKeys where KeyId = " + id + " and isActive = 'Active'";
+		Timestamp timestamp = null;
+		Date createdAt = null, expiresAt = null;
 		try {
 			conn = DBContext.getInstance().getConnection();
 			ps = conn.prepareStatement(query);
-			ps.setInt(1, id);
 			rs = ps.executeQuery();
 			while (rs.next()) {
-				return new Category(rs.getInt(1), rs.getString(2));
+				timestamp = rs.getTimestamp("CreatedAt");
+				if (timestamp != null) {
+					createdAt = new Date(timestamp.getTime());
+				}
+				timestamp = rs.getTimestamp("ExpiresAt");
+				if (timestamp != null) {
+					expiresAt = new Date(timestamp.getTime());
+				}
+				boolean isActive = rs.getString("IsActive").equalsIgnoreCase("Active") ? true : false;
+				return new UserKey(Integer.toString(id), rs.getString("PublicKey"), rs.getString("KeyType"),
+						rs.getInt("KeySize"), createdAt, expiresAt, isActive);
 			}
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.out.println(e);
 		}
 		return null;
 	}
 
+	public int createKey(String userId, String publicKey, String keyType, int keySize, long expireTime) {
+		String query = "insert into UserPublicKeys(UserId, PublicKey, KeyType, KeySize, CreatedAt, ExpiresAt, IsActive)"
+				+ " values (?, ?, ?, ?, ?, ?, ?)";
+		try {
+			conn = DBContext.getInstance().getConnection();
+			ps = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, userId);
+			ps.setString(2, publicKey);
+			ps.setString(3, keyType);
+			ps.setInt(4, keySize);
+			LocalDateTime now = LocalDateTime.now();
+			Timestamp timestampCreate = Timestamp.valueOf(now);
+			ps.setTimestamp(5, timestampCreate);
+			LocalDateTime expireDate = now.plusDays(expireTime);
+			Timestamp timestampExpire = Timestamp.valueOf(expireDate);
+			ps.setTimestamp(6, timestampExpire);
+			ps.setString(7, "Active");
+			int affectedRows = ps.executeUpdate();
+			if (affectedRows == 0) {
+				throw new SQLException("Creating user failed, no rows affected.");
+			}
+			try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+				if (generatedKeys.next()) {
+					int id = generatedKeys.getInt(1);
+					// Trả về đối tượng User vừa tạo
+					return id;
+				} else {
+					throw new SQLException("Creating user failed, no ID obtained.");
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return -1;
+	}
+
+	public int createKey(String userId, String publicKey, String keyType, int keySize) {
+		String query = "insert into UserPublicKeys(UserId, PublicKey, KeyType, KeySize, CreatedAt, ExpiresAt, IsActive)"
+				+ " values (?, ?, ?, ?, ?, ?, ?)";
+		try {
+			conn = DBContext.getInstance().getConnection();
+			ps = conn.prepareStatement(query);
+			ps.setString(1, userId);
+			ps.setString(2, publicKey);
+			ps.setString(3, keyType);
+			ps.setInt(4, keySize);
+			LocalDateTime now = LocalDateTime.now();
+			Timestamp timestampCreate = Timestamp.valueOf(now);
+			ps.setTimestamp(5, timestampCreate);
+			Timestamp expireDate = new Timestamp(9999, 12, 31, 23, 59, 59, 00);
+			ps.setTimestamp(6, expireDate);
+			ps.setString(7, "Active");
+			int affectedRows = ps.executeUpdate();
+			if (affectedRows == 0) {
+				throw new SQLException("Creating user failed, no rows affected.");
+			}
+			try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
+				if (generatedKeys.next()) {
+					int id = generatedKeys.getInt(1);
+					// Trả về đối tượng User vừa tạo
+					return id;
+				} else {
+					throw new SQLException("Creating user failed, no ID obtained.");
+				}
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return -1;
+	}
+
 	public static void main(String[] args) {
 		KeyDAO dao = new KeyDAO();
-		System.out.println(dao.getCategoryById(2).toString());
+		dao.createKey("2", "123", "DSA", 1024, 5);
 	}
 }
